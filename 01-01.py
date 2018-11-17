eefacb91563b0247c83747caba8d1a2051e3a16e
liberbell <liberbell@gmail.com>
2018-11-17 11:05:04 +0900

    bool3

--- 01-01.py
+++ 01-01.py
@@ -1,5 +1,8 @@
 isRaining = True
-isSunny = True
+isSunny = False
 
 if isRaining and isSunny:
     print('We might see a rainbow')
+
+if isRaining or isSunny:
+    print('It might be rainy or it might be sunny')
