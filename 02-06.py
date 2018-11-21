f352bf814555f003f1bf89dfa0c518b0abb16e31
liberbell <liberbell@gmail.com>
2018-11-22 08:46:45 +0900

    iterator3

--- 02-06.py
+++ 02-06.py
@@ -3,3 +3,6 @@ import itertools
 election = {1: 'Barb', 2: 'Karen', 3: 'Erin'}
 for p in itertools.permutations(election):
     print(p)
+
+for p1 in itertools.permutations(election.values()):
+    print(p1)
