e4276436e90720521e9474384d08ce0744a4de9b
liberbell <liberbell@gmail.com>
2018-11-22 08:38:27 +0900

    itertools6

--- 02-05.py
+++ 02-05.py
@@ -4,3 +4,10 @@ for x in itertools.count(50, 5):
     print(x)
     if x == 80:
         break;
+
+x = 0
+for c in itertools.cycle('RACECAR'):
+    print(c)
+    x = x + 1
+    if x > 50:
+        break;
