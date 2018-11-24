4012f849f8a956f862ad9bf39c9956e6ecbd7fec
liberbell <liberbell@gmail.com>
2018-11-24 09:16:36 +0900

    files

--- 03-03.py
+++ 03-03.py
@@ -5,3 +5,7 @@ print('Mode ' + myFiles.mode)
 
 myFiles.write('GBJ: 100\nKHD: 99\nBBB: 89')
 myFiles.close()
+
+myFile2 = open('scores.txt', 'r')
+print('Reading...' + myFile2.read())
+myFile2.close()
