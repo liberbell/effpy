26ce6b03d922a30745b4488e4236ad443e4ed503
liberbell <liberbell@gmail.com>
2018-11-24 09:17:59 +0900

    files2

--- 03-03.py
+++ 03-03.py
@@ -3,9 +3,9 @@ myFiles = open('scores.txt', 'w')
 print('Name ' + myFiles.name)
 print('Mode ' + myFiles.mode)
 
-myFiles.write('GBJ: 100\nKHD: 99\nBBB: 89')
+myFiles.write('GBJ : 100\nKHD : 99\nBBB : 89')
 myFiles.close()
 
 myFile2 = open('scores.txt', 'r')
-print('Reading...' + myFile2.read())
+print('Reading...\n' + myFile2.read(10))
 myFile2.close()
