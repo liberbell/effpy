4906909c949ca995fa06d6ad05ce0fdba597d09b
liberbell <liberbell@gmail.com>
2018-11-24 09:42:54 +0900

    line2

--- 03-05.py
+++ 03-05.py
@@ -2,3 +2,11 @@ myFile = open('scores.txt', 'r')
 
 print('My one line: ' + myFile.readline())
 print('My one line: ' + myFile.readline())
+myFile.seek(0)
+
+for line in myFile:
+    newHighScore = line.replace('BBB', 'PDJ')
+    print(line)
+    print(newHighScore)
+
+myFile.close()
