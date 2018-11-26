9d39e947068cb23cf99c753cb1577e9d5360ff4c
liberbell <liberbell@gmail.com>
2018-11-27 07:51:04 +0900

    parser4

--- 04-05.py
+++ 04-05.py
@@ -2,4 +2,18 @@ from html.parser import HTMLParser
 
 class HTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print('Start tag')
+        print('Start tag: ', tag)
+        for attr in attrs:
+            print('attrs: ', attr)
+
+    def handle_endtag(self, tag):
+        print('End tag: ', tag)
+
+    def handle_comment(self, data):
+        print('Comment: ', data)
+
+    def handle_data(self, data):
+        print('Data: ', data)
+
+parser = HTMLParser()
+parser.feed('<html><head><title>Coder</title></head><body><h1><!--h1-->I am a coder</h1></body></html>')
