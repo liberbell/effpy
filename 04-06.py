1a797b38b011f5e8c2749f0239b66be511a69a38
liberbell <liberbell@gmail.com>
2018-11-28 18:29:08 +0900

    textwrap1

---
+++ 04-06.py
@@ -0,0 +1,9 @@
+import textwrap
+
+websiteText = '''
+Leaving can happen anywhere with our apps on your computer,
+mobile device, and TV, featuring enhanced navigation and farstr
+streaming for anytime leaving.'''
+
+print('No Dedent:')
+print(textwrap.websiteText)
